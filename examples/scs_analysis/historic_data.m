0b23870b8e1d1479a04e010db29a13f497d02157
Milton Logothets <mihrilo1@hotmail.com>
2019-03-21 18:01:25 +0000

    added documentation and cleaned

--- examples/scs_analysis/historic_data.m
+++ examples/scs_analysis/historic_data.m
@@ -1,3 +1,28 @@
+% HISTORIC DATA IMPORTER & PLOTTER
+%
+% Created 20 December 2018
+%
+%@author Milton Logothetis (milton.logothetis@southcoastscience.com)
+%
+% DESCRIPTION
+% This script utilises AWS scs_analysis tools in order to import and plot
+% raw historic data from a subscribed topic. 
+% 
+% EXAMPLE
+% 1. Specify user-defined variables.
+% 2. Specify parameters to be imported and plotted off subscribed topic:
+% 
+% fmt: {type.data."parameter_name"(var.i,1) = jsondecode(var.i)."JSON_field"}
+% 
+% 3. Specify which parameters to plot within Y_data structure:
+% 
+% fmt (2D): {Y_data."decoded_parameter_name" = [];}
+%
+% 4. Choose plot type
+% 
+% SEE ALSO
+% utilities.multiplot (multiple 2D plot)
+
 clearvars;
 
 filename = mfilename('fullpath');
@@ -7,10 +32,13 @@ filename = mfilename('fullpath');
 var.Topic_ID = 'unep/ethiopia/loc/1/particulates';
 var.start_time = '2018-12-24T00:00:00Z';
 var.end_time = '2018-12-24T01:00:00Z';
-%--------------------------------------------------------------------------
-jsondecode = utilities.decode_fcn(var);
-var.sample_length = length(jsondecode);
 
+% Initialization
+jsondecode = utilities.decode_fcn(var); % Import parameters in JSON format. 
+var.sample_length = length(jsondecode);
+hist.last_update = [1;0]; % Indicate data type is raw data.
+
+% Decode parameters from JSON format
 for i = 1:var.sample_length
     var.i = i;
     type.data.datetime{var.i,1} = jsondecode(var.i).rec;
@@ -20,6 +48,10 @@ for i = 1:var.sample_length
 end
 
 live_fig = figure('Name', 'Historic Data');
-Y_data.PM10 = []; Y_data.PM2p5 = []; Y_data.PM1 = []; % Specify parameters to plot.
-multiplot(Y_data, type, var, jsondecode, live_fig);
-legend('PM1', 'PM2.5', 'PM10')
+
+% Specify parameters for 2D plot.
+% Y_data.PM10 = []; Y_data.PM2p5 = []; Y_data.PM1 = []; 
+% utilities.multiplot(Y_data, type, hist, var, jsondecode, live_fig);
+
+Z_data = type.data.PM10;
+utilities.ddd_surf_plot(Z_data, type, var)
--- examples/scs_analysis/live_data.m
+++ examples/scs_analysis/live_data.m
@@ -1,3 +1,26 @@
+% LIVE DATA IMPORTER & PLOTTER
+%
+% Created 20 December 2018
+%
+%@author Milton Logothetis (milton.logothetis@southcoastscience.com)
+%
+% DESCRIPTION
+% This script utilises AWS scs_analysis tools in order to import and plot
+% raw live data from a subscribed topic. 
+% 
+% EXAMPLE
+% 1. Specify user-defined variables.
+% 2. Specify parameters to be imported and plotted off subscribed topic:
+% 
+% fmt: {type.data."parameter_name"(var.i,1) = jsondecode(var.i)."JSON_field"}
+% 
+% 3. Specify which parameters to plot within Y_data structure:
+% 
+% fmt (2D): {Y_data."decoded_parameter_name" = [];}
+% 
+% SEE ALSO
+% utilities.multiplot (multiple 2D plot)
+
 clearvars;
 
 filename = mfilename('fullpath');
@@ -6,10 +29,12 @@ filename = mfilename('fullpath');
 % User-defined variables:
 var.Topic_ID = 'south-coast-science-dev/production-test/loc/1/gases'; % Specify subscription topic.
 sampling_rate = 10;                                                   % Specify sensor's sampling rate in seconds.
-%-------------------------------------------------------------------------------------------------------------
+
 % Initialization
-var.start_time = utilities.time_init(var);
+var.start_time = utilities.time_init(var.Topic_ID);
 var.i = 0;
+hist.last_update = [1;0]; % Indicate data type is raw data.
+
 while (1)
     pause(sampling_rate);
     if var.i==0
@@ -21,7 +46,8 @@ while (1)
         var.start_time = type.data.datetime{end};
     end
     jsondecode{var.j,1} = utilities.decode_fcn(var);
-   
+    
+    % Decode parameters from JSON format:
     for n = 1:length(jsondecode{end})
         type.data.datetime{var.i+n-1,1} = jsondecode{var.j,1}(n).rec;
         type.data.NO2(var.i+n-1,1) = jsondecode{var.j,1}(n).val.NO2.cnc;
@@ -33,8 +59,10 @@ while (1)
     end
     
     if var.i==1
-        live_fig = figure('Name', 'Live Data');
+        live_fig = figure('Name', 'Live Data'); % Initialize figure.
     end
-    Y_data.NO2 = []; Y_data.tmp =[]; % Specify plotted parameters in shown format.
-    multiplot(Y_data, type, var, jsondecode, live_fig);
+    
+    % Specify parameters for 2D plot:
+    Y_data.NO2 = []; Y_data.SO2 =[]; Y_data.tmp =[]; Y_data.hmd=[];
+    [live_fig,properties,mplt] = utilities.multiplot(Y_data, type, hist, var, jsondecode, live_fig);
 end
